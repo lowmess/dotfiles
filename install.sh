f340a7ccdf7a046da6a9def61e483ef76218dadd
Alec Lomas <alec@lowmess.com>
2017-11-08 23:12:40 -0700

    :hammer: use HOME in place of ~
    except in the README, idk

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 # Install things with Homebrew
-source "$HOME/dotfiles/brew/brew.sh"
+source $HOME/dotfiles/brew/brew.sh
 
 # Set up nvm
-mkdir -p ~/.nvm
+mkdir -p $HOME/.nvm
 nvm install 8
 nvm alias default 8
 
@@ -11,14 +11,14 @@ source $HOME/dotfiles/system/.function
 npmglobals
 
 # create config files
-touch ~/.zshrc
-echo 'source $HOME/dotfiles/.zshrc' | cat - ~/.zshrc | sponge ~/.zshrc
+touch $HOME/.zshrc
+echo 'source $HOME/dotfiles/.zshrc' | cat - $HOME/.zshrc | sponge $HOME/.zshrc
 
-touch ~/.gitconfig
-echo '[include]\n  path = ~/dotfiles/git/.gitconfig' | cat - ~/.gitconfig | sponge ~/.gitconfig
+touch $HOME/.gitconfig
+echo '[include]\n  path = $HOME/dotfiles/git/.gitconfig' | cat - $HOME/.gitconfig | sponge $HOME/.gitconfig
 
 source $HOME/dotfiles/system/.alias
 update-hyper
 
-touch ~/.czrc
-echo '{\n  "path": "cz-conventional-changelog"\n}' | cat - ~/.czrc | sponge ~/.czrc
+touch $HOME/.czrc
+echo '{\n  "path": "cz-conventional-changelog"\n}' | cat - $HOME/.czrc | sponge $HOME/.czrc
