f9ab44faaee8f205d78c38be15c11161cee91b51
Alec Lomas <alec@lowmess.com>
2019-03-29 05:22:16 -0700

    :art: simplify install script
    also fix some stuff, bump default node version, etc

--- brew/brew.sh
+++ brew/brew.sh
@@ -1,5 +1,6 @@
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # use brewfile to install a bunch of stuff
 brew tap homebrew/bundle
 brew bundle
--- install.sh
+++ install.sh
@@ -1,24 +1,23 @@
-# Install things with Homebrew
+# Set up zsh
+touch $HOME/.zshrc
+echo 'source $HOME/dotfiles/.zshrc' | cat - $HOME/.zshrc | sponge $HOME/.zshrc
+source $HOME/.zshrc
+
+# Install Homebrew & Homebrew packages
 source $HOME/dotfiles/brew/brew.sh
 
 # Set up nvm
 mkdir -p $HOME/.nvm
-nvm install 8
-nvm alias default 8
+nvm install 10
+nvm alias default 10
 
 # Install global Node deps
-source $HOME/dotfiles/system/.function
-npmglobals
+npm-globals
 
-# create config files
-touch $HOME/.zshrc
-echo 'source $HOME/dotfiles/.zshrc' | cat - $HOME/.zshrc | sponge $HOME/.zshrc
-
+# create git config
 touch $HOME/.gitconfig
 echo '[include]\n  path = $HOME/dotfiles/git/.gitconfig' | cat - $HOME/.gitconfig | sponge $HOME/.gitconfig
 
-source $HOME/dotfiles/system/.alias
-update-hyper
-
-touch $HOME/.czrc
-echo '{\n  "path": "cz-conventional-changelog"\n}' | cat - $HOME/.czrc | sponge $HOME/.czrc
+# update hyper config
+touch $HOME/.hyper.js
+cp $HOME/dotfiles/.hyper.js $HOME/.hyper.js
