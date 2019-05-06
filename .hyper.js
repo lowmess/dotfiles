97aa1509d8e16e7d9f858fc58db2279c92aefcb2
Alec Lomas <alec@lowmess.com>
2019-05-06 04:58:51 -0700

    :wrench: add new hyper default config option

--- .hyper.js
+++ .hyper.js
@@ -140,6 +140,11 @@ module.exports = {
     // selection is present (`true` by default on Windows and disables the context menu feature)
     // quickEdit: true,
 
+    // choose either `'vertical'`, if you want the column mode when Option key is hold during selection (Default)
+    // or `'force'`, if you want to force selection regardless of whether the terminal is in mouse events mode
+    // (inside tmux or vim with mouse mode enabled for example).
+    macOptionSelectionMode: "vertical",
+
     // URL to custom bell
     // bellSoundURL: 'http://example.com/bell.mp3',
 
